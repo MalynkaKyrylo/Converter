a8a1dcbcb37809a0df54bebd07ca6ca6b2c9d002
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-06-12 22:19:47 +0300

    add feet <-> centimeters converter

--- src/converter/Main.java
+++ src/converter/Main.java
@@ -9,11 +9,23 @@ public class Main {
         double kilometers = 10;
         double milesConv1 = kilometersToMiles(kilometers);
         System.out.println(kilometers + " километров = " + milesConv1 + " миль");
+        double feet = 6;
+        double centimetersConv1 = feetToCentimeters(feet);
+        System.out.println(feet + " футов = " + centimetersConv1 + " сантиметров");
+        double centimeters = 170;
+        double feetConv1 = centimetersToFeet(centimeters);
+        System.out.println(centimeters + " сантиметров = " + feetConv1 + " футов");
     }
     public static double milesToKilometers(double miles) {
         return miles * 1.60934;
     }
     public static double kilometersToMiles(double kilometers) {
         return kilometers / 1.60934;
+    }
+    public static double feetToCentimeters(double feet) {
+        return feet * 30.48;
+    }
+    public static double centimetersToFeet(double centimeters) {
+        return centimeters / 30.48;
     }
 }
