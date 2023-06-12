c899e63b4d2032ee9ac1de570c2fced5c4def587
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-06-12 22:14:09 +0300

    add miles <-> kilometres converter

--- src/converter/Main.java
+++ src/converter/Main.java
@@ -3,5 +3,17 @@ package converter;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Converter app");
+        double miles = 5;
+        double kilometersConv1 = milesToKilometers(miles);
+        System.out.println(miles + " миль = " + kilometersConv1 + " километров");
+        double kilometers = 10;
+        double milesConv1 = kilometersToMiles(kilometers);
+        System.out.println(kilometers + " километров = " + milesConv1 + " миль");
+    }
+    public static double milesToKilometers(double miles) {
+        return miles * 1.60934;
+    }
+    public static double kilometersToMiles(double kilometers) {
+        return kilometers / 1.60934;
     }
 }
