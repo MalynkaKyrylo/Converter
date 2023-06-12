aefc943a919dd66eec546bfd3ef93238f8129add
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-06-12 22:18:33 +0300

    add inches <-> millimeters converter

--- src/converter/Main.java
+++ src/converter/Main.java
@@ -9,11 +9,24 @@ public class Main {
         double kilometers = 10;
         double milesConv1 = kilometersToMiles(kilometers);
         System.out.println(kilometers + " километров = " + milesConv1 + " миль");
+        double inches = 10;
+        double millimetersConv1 = inchesToMillimeters(inches);
+        System.out.println(inches + " дюймов = " + millimetersConv1 + " миллиметров");
+        double millimeters = 100;
+        double inchesConv1 = millimetersToInches(millimeters);
+        System.out.println(millimeters + " миллиметров = " + inchesConv1 + " дюймов");
     }
     public static double milesToKilometers(double miles) {
         return miles * 1.60934;
     }
     public static double kilometersToMiles(double kilometers) {
         return kilometers / 1.60934;
+    }
+    public static double inchesToMillimeters(double inches) {
+        return inches * 25.4;
+    }
+    public static double millimetersToInches(double millimeters) {
+        return millimeters / 25.4;
+
     }
 }
